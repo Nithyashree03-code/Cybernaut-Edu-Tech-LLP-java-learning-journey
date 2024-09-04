66dae38c6f5cf5c45db32945cf0d09ed4433bf4b
Nithyashree03-code <nshree03112005@gmail.com>
2024-09-04 08:42:20 +0530

    Assignment added

---
+++ Day 8/recussion.java
@@ -0,0 +1,10 @@
+public class recussion {
+    public static int calculateSum(int a, int b) {
+        int result = a + b; 
+        return result;
+    }
+    public static void main(String[] args) {
+    int sum = calculateSum(5, 3); 
+    System.out.println(sum); 
+    }
+      }
