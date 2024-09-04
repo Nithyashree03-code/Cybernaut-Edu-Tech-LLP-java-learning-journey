25f48005b0be5ae70a035cf56e19b9541785a4eb
Nithyashree03-code <nshree03112005@gmail.com>
2024-09-04 08:36:52 +0530

    Assignment added

--- Day 7/Add.java
+++ Day 7/Add.java
@@ -1,1 +1,1 @@
-public class Add { public static int add(int a, int b) { return a + b; } public static void main(String[] args) { System.out.println(add(5, 3)); } }
+public class Add { public static void greet(String name) { System.out.println("Hello, " + name + "!"); } public static void main(String[] args) { greet("harivarshan"); } }
